8513d7c9daf002986fd06c3116b320a64a55493b
shogoism <sadowsky3655@gmail.com>
2022-09-16 15:08:00 +0900

    Update main.go

--- main.go
+++ main.go
@@ -128,6 +128,7 @@ func forwardRequest(req *http.Request, reqSourceIP string, reqDestionationPort s
 
 	// create a new url from the raw RequestURI sent by the client
 	url := fmt.Sprintf("%s%s", string(fwdMap[reqSourceIP]), req.RequestURI)
+	log.Print(url)
 
 	// create a new HTTP request
 	forwardReq, err := http.NewRequest(req.Method, url, bytes.NewReader(body))
